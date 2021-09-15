bbec2c6fd86f463d7f54adfd9358f7b0d0753217
Nicolas Rios <nicolasq2929@gmail.com>
2021-09-15 14:48:10 -0500

    Login implemented

---
+++ src/ui/Main.java
@@ -0,0 +1,41 @@
+package ui;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	private LoginGUI loginGUI;
+	
+	public Main() throws IOException {
+		loginGUI = new LoginGUI();
+	}
+	
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Login.fxml"));
+		fxmlLoader.setController(loginGUI);
+		
+		Parent root = fxmlLoader.load();
+		
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Clover Company");
+		primaryStage.setResizable(false);
+		primaryStage.show();
+		
+	}
+
+}
