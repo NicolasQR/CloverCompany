3a65f7552ee544cfcad4c127890619715f3535e0
DaniloErazo <erazodanilo1218@gmail.com>
2021-09-15 14:56:12 -0500

    Controller for MainPage.fxml created, functions in construction. Ready to merge with other screens created

---
+++ src/ui/MainPageController.java
@@ -0,0 +1,34 @@
+package ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+public class MainPageController {
+
+	@FXML
+    private Button userInfo;
+
+    @FXML
+    private Button addClient;
+
+    @FXML
+    private Button searchRegion;
+
+    @FXML
+    private Button searchId;
+
+    @FXML
+    private BorderPane paneContents;
+
+    @FXML
+    private BorderPane paneOverview;
+
+    @FXML
+    void loadContents(ActionEvent event) {
+    	
+    }
+
+	
+}
